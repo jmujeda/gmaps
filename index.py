ee0aced05254e14efdf93927ad5093a1d9450e49
uje <jmujeda@gmail.com>
2017-01-22 14:26:33 -0300

    Dibuja los recorridos de todas las lineas

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 #!C:\Python36\python.exe
-
+from __future__ import print_function
+import sys
 
 from flask import Flask, render_template
 import json
@@ -13,31 +14,41 @@ def bondis():
 	res = {}
 	lineas = []
 	terminales = []
+	recorridos = []
 	count = 0
 	for register in f:
 		cols = register.split(';')
-		linea = cols[2][1:len(cols[2]) - 1]  # las lineas vienen con comillas
+		linea = get_linea(cols[2])
 		add_linea(lineas, linea)
 
-		recorrido = cols[0]
-		lngLats = recorrido[13:len(recorrido) - 2].split(',')
-		lngLat = lngLats[0].split()
+		
 
+		lngLats = cols[0][13:len(cols[0]) - 2].split(',')
+		lngLat = lngLats[0].split()
 		sentido = cols[5]
 		res["sentido"] = sentido
-		if 'IDA' in sentido:
-			add_terminal(terminales, lngLat, linea)
 
-		if linea == '39':
-			recorrido = cols[0]
-			lngLat = recorrido[13:len(recorrido) - 2].split(',')
-			res['linea39'] = repr(lngLat[0])
+		#if 'IDA' in sentido:
+			#add_terminal(terminales, lngLat, linea)
 
+		#if linea == '132':
+		recorrido = get_recorrido(cols[0])
+		recorridos.append(recorrido)
+			#recorrido = cols[0]
+			#lngLat = recorrido[13:len(recorrido) - 2].split(',')
+			#res['linea39'] = repr(lngLat[0])
 		count += 1
+	res['recorridos'] = recorridos
 	res['terminales'] = terminales
 	res['cant_lineas'] = len(lineas)
 	res['lineas'] = lineas
+	#print(res['lineas'], file=sys.stderr)
 	return json.JSONEncoder().encode(res)
+
+
+# Saca las comillas del numero de linea
+def get_linea(col):
+	return col[1:len(col) - 1]
 
 
 def add_linea(lineas, linea):
@@ -46,8 +57,17 @@ def add_linea(lineas, linea):
 	return
 
 
-def add_recorrido():
-	return
+def get_recorrido(colRecorrido):
+	recorrido = []
+	lngLats = colRecorrido[13:len(colRecorrido) - 2].split(',')
+	for lngLat in lngLats:
+		lngLat = lngLat.split()
+		parada = {'lng': float(lngLat[0]), 'lat': float(lngLat[1])}
+		#print(parada, file=sys.stderr)
+
+		recorrido.append(parada)
+
+	return recorrido
 
 
 def add_terminal(terminales, lngLat, linea):
